636e138c5a56fc7f241e303503e62b95cae6b512
Marek Fajkus <marek.faj@gmail.com>
2019-07-06 19:48:10 +0200

    optimize implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 mod utils;
 
-use std::fmt;
 use wasm_bindgen::prelude::*;
+
+extern crate fixedbitset;
+use fixedbitset::FixedBitSet;
 
 #[wasm_bindgen]
 #[repr(u8)]
@@ -15,7 +17,7 @@ pub enum Cell {
 pub struct Universe {
     width: u32,
     height: u32,
-    cells: Vec<Cell>,
+    cells: FixedBitSet,
 }
 
 #[wasm_bindgen]
@@ -24,25 +26,18 @@ impl Universe {
         let width = 64;
         let height = 64;
 
-        let cells = (0..width * height)
-            .map(|i| {
-                if i % 2 == 0 || i % 7 == 0 {
-                    Cell::Alive
-                } else {
-                    Cell::Dead
-                }
-            })
-            .collect();
+        let size = (width * height) as usize;
+        let mut cells = FixedBitSet::with_capacity(size);
+
+        for i in 0..size {
+            cells.set(i, i % 2 == 0 || i % 7 == 0);
+        }
 
         Universe {
             width,
             height,
             cells,
         }
-    }
-
-    pub fn render(&self) -> String {
-        self.to_string()
     }
 
     pub fn tick(&mut self) {
@@ -54,20 +49,18 @@ impl Universe {
                 let cell = self.cells[idx];
                 let living_neighbours = self.live_neighbour_count(row, col);
 
-                let next_cell = match (cell, living_neighbours) {
+                next.set(idx, match (cell, living_neighbours) {
                     // Any living cell with fewer than two living neighbours dies
-                    (Cell::Alive, x) if x < 2 => Cell::Dead,
+                    (true, x) if x < 2 => false,
                     // Any living cell with two or three living heighbours lives
-                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                    (true, 2) | (true, 3) => true,
                     // Any living cell with more than three living neighbours dies
-                    (Cell::Alive, x) if x > 3 => Cell::Dead,
+                    (true, x) if x > 3 => false,
                     // Any dead cell with exactly three living neighbours lives
-                    (Cell::Dead, 3) => Cell::Alive,
+                    (false, 3) => true,
                     // All remining stay the same
                     (otherwise, _) => otherwise,
-                };
-
-                next[idx] = next_cell;
+                });
             };
         };
 
@@ -82,8 +75,8 @@ impl Universe {
         self.height
     }
 
-    pub fn cells(&self) -> *const Cell {
-        self.cells.as_ptr()
+    pub fn cells(&self) -> *const u32 {
+        self.cells.as_slice().as_ptr()
     }
 
     fn get_index(&self, row: u32, column: u32) -> usize {
@@ -105,20 +98,6 @@ impl Universe {
             }
         }
         count
-    }
-}
-
-impl fmt::Display for Universe {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        for line in self.cells.as_slice().chunks(self.width as usize) {
-            for &cell in line {
-                let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
-                write!(f, "{}", symbol)?;
-            }
-            write!(f, "\n")?;
-        }
-
-        Ok(())
     }
 }
 
